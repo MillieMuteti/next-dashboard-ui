9123026f93eefc3bd995ced759a534391e27871e
Millie Muteti <milliemuteti@gmail.com>
2025-02-11 16:56:09 +0300

    Create navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import Menu from "../../components/Menu";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -22,7 +23,10 @@ export default function DashboardLayout({
       </div>
 
       {/* right*/}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">R</div>
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+        </div>
     </div>
   );
 }
