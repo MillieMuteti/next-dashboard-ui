705b26c6bdd39a67069b4613e957b3072d2fb88d
Millie Muteti <milliemuteti@gmail.com>
2025-02-11 16:05:58 +0300

    create menu panel

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "../../components/Menu";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* Left */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4 mt-4 text-sm">
+        <Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
+        <Image src="/logo.png" alt="logo" width={32} height={32} />
+        <span className="hidden lg:block">Graceland
+
+        </span>
+
+        </Link>
+        <Menu />
+      </div>
+
+      {/* right*/}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]">R</div>
+    </div>
+  );
+}
