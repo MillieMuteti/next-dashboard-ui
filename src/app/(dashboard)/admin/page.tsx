d9431ad91d4f108570e26d9bd9d8240af7977aca
Millie Muteti <milliemuteti@gmail.com>
2025-02-11 22:54:02 +0300

    create radial chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,11 @@
+import CountChart from "@/components/CountChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
   return (
     <div className="p-4 gap-4 flex flex-col md:flex-row">
      {/* LEFT */}
-     <div className="w-full lg:w-2/3">
+     <div className="w-full lg:w-2/3 flex flex-col gap-8">
      {/* USER CARDS */}
      <div className="flex gap-4 justify-between flex-wrap">
       <UserCard type="student"/>
@@ -13,6 +14,21 @@ const AdminPage = () => {
       <UserCard type="staff"/>
 
      </div>
+     {/* MIDDLE CHARTS */}
+     <div className="flex gap-4 flex-col lg:flex-row">
+
+      {/* Count Chart */}
+      <div className="w-full lg:w-1/3 h-[450px]">
+      <CountChart />
+      </div>
+
+      {/* Attendance Chart */}
+      <div className="w-full lg:w-2/3 h-[450px]"></div>
+
+
+     </div>
+     {/* Bottom Chart */}
+     <div></div>
      
      </div>
 
