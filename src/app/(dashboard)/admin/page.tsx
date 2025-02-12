014a8bb1a56b7710a5db2c8c8a29378fa589a669
Millie Muteti <milliemuteti@gmail.com>
2025-02-12 12:42:51 +0300

    Create finance Chart

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,12 +1,13 @@
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
   return (
     <div className="p-4 gap-4 flex flex-col md:flex-row">
      {/* LEFT */}
-     <div className="w-full lg:w-2/3 flex flex-col gap-8">
+     <div className="w-full lg:w-2/3 flex flex-col gap-8 ">
      {/* USER CARDS */}
      <div className="flex gap-4 justify-between flex-wrap">
       <UserCard type="student"/>
@@ -31,7 +32,9 @@ const AdminPage = () => {
 
      </div>
      {/* Bottom Chart */}
-     <div></div>
+     <div className="w-full h-[500px]">
+      <FinanceChart />
+     </div>
      
      </div>
 
