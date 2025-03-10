694ed7bb5248e32cf98781f39db39459064d1c21
Millie Muteti <milliemuteti@gmail.com>
2025-03-10 13:11:17 +0300

    add lesson subject and exam list

--- src/app/(dashboard)/list/classes/page.tsx
+++ src/app/(dashboard)/list/classes/page.tsx
@@ -4,22 +4,34 @@ import Image from "next/image";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import Link from "next/link";
-import { role, subjectsData } from "@/lib/data";
+import { classesData, role, subjectsData } from "@/lib/data";
 
-type Subject = {
+type Class = {
   id: number;
   name: string;
-  teachers: string[];
+  supervisor: string;
+  grade:number;
+  capacity:number;
 };
 
 const columns = [
   {
-    header: "Subject Name",
-    accesor: "subjectName",
+    header: "Class Name",
+    accesor: "className",
   },
   {
-    header: "Teachers",
-    accesor: "teachers",
+    header: "Capacity",
+    accesor: "capacity",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Grade",
+    accesor: "grade",
+    className: "hidden md:table-cell",
+  },
+  {
+    header: "Supervisor",
+    accesor: "supervisor",
     className: "hidden md:table-cell",
   },
 
@@ -29,20 +41,22 @@ const columns = [
   },
 ];
 
-const SubjectListPage = () => {
-  const renderRows = (item: Subject) => (
+const ClassListPage = () => {
+  const renderRows = (item: Class) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:border-slate-50 text-xs hover:bg-lamaPurpleLight"
     >
       <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.teachers.join(", ")}</td>
+      <td className="hidden md:table-cell">{item.capacity}</td>
+      <td className="hidden md:table-cell">{item.grade}</td>
+      <td className="hidden md:table-cell">{item.supervisor}</td>
 
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" alt="" height={16} width={16} />
+              <Image src="/edit.png" alt="" height={16} width={16} />
             </button>
           </Link>
           {role === "admin" && (
@@ -58,7 +72,7 @@ const SubjectListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* Top */}
       <div className="flex justify-between items-center">
-        <h1 className=" hidden md:block text-lg font-semibold">All Subjects</h1>
+        <h1 className=" hidden md:block text-lg font-semibold">All Classes</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center self-end gap-4">
@@ -78,7 +92,7 @@ const SubjectListPage = () => {
       </div>
 
       {/* List */}
-      <Table columns={columns} renderRows={renderRows} data={subjectsData} />
+      <Table columns={columns} renderRows={renderRows} data={classesData} />
 
       {/* Pagination*/}
 
@@ -87,4 +101,4 @@ const SubjectListPage = () => {
   );
 };
 
-export default SubjectListPage;
+export default ClassListPage;
--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -4,45 +4,53 @@ import Image from "next/image";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import Link from "next/link";
-import { role, subjectsData } from "@/lib/data";
+import {  lessonsData, role,  } from "@/lib/data";
 
-type Subject = {
+type Lesson = {
   id: number;
-  name: string;
-  teachers: string[];
+  subject: string;
+  teacher: string;
+  class:string;
+ 
 };
 
 const columns = [
   {
     header: "Subject Name",
-    accesor: "subjectName",
+    accesor: "subject",
   },
   {
-    header: "Teachers",
-    accesor: "teachers",
+    header: "Class",
+    accesor: "class",
     className: "hidden md:table-cell",
   },
-
+  {
+    header: "Teacher",
+    accesor: "teacher",
+    className: "hidden md:table-cell",
+  },
+ 
   {
     header: "Actions",
     accesor: "actions",
   },
 ];
 
-const SubjectListPage = () => {
-  const renderRows = (item: Subject) => (
+const LessonListPage = () => {
+  const renderRows = (item: Lesson) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:border-slate-50 text-xs hover:bg-lamaPurpleLight"
     >
-      <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.teachers.join(", ")}</td>
-
+      <td className="flex items-center gap-4 p-4">{item.subject}</td>
+      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
+      
       <td>
         <div className="flex items-center gap-2">
           <Link href={`/list/teachers/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-              <Image src="/view.png" alt="" height={16} width={16} />
+              <Image src="/edit.png" alt="" height={16} width={16} />
             </button>
           </Link>
           {role === "admin" && (
@@ -58,7 +66,7 @@ const SubjectListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* Top */}
       <div className="flex justify-between items-center">
-        <h1 className=" hidden md:block text-lg font-semibold">All Subjects</h1>
+        <h1 className=" hidden md:block text-lg font-semibold">All Lessons</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center self-end gap-4">
@@ -78,7 +86,7 @@ const SubjectListPage = () => {
       </div>
 
       {/* List */}
-      <Table columns={columns} renderRows={renderRows} data={subjectsData} />
+      <Table columns={columns} renderRows={renderRows} data={lessonsData} />
 
       {/* Pagination*/}
 
@@ -87,4 +95,4 @@ const SubjectListPage = () => {
   );
 };
 
-export default SubjectListPage;
+export default LessonListPage;
