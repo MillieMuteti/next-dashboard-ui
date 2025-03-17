66f5eeb78e151ff49a4c8fee28b44906bbde40dd
Millie Muteti <milliemuteti@gmail.com>
2025-03-17 20:01:45 +0300

    create delete form

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Image from "next/image";
+import { useState } from "react";
 
 const FormModal = ({
   table,
@@ -31,13 +32,35 @@ const FormModal = ({
       : type === "update"
       ? "bg-lamaSky"
       : "bg-lamaPurple";
+
+  const [open, setOpen] = useState(false);
+
+  const Form = () => {
+    return type === "delete" && id ? (<form action="" className="p-4 flex flex-col gap4">
+      <span className="text-center font-medium">All data will be lost. Are you sure you want to delete this {table}?</span>
+      <button className="bg-red-700 text-white py-2 px-4 rounded-md border-none  w-max self-center mt-4">Delete</button>
+
+
+    </form>) : "create or update form"
+  }
   return (
     <div>
       <button
         className={`${size} flex items-center justify-center rounded-full ${bgColour}`}
+        onClick={() => setOpen(true)}
       >
         <Image src={`/${type}.png`} alt="" width={16} height={16} />
       </button>
+      {open && (
+        <div className="w-screen h-screen absolute top-0 left-0 bg-black bg-opacity-60 z-50 flex items-center justify-center">
+          <div className="bg-white rounded-md p-4 relative w-[90%] md:w=[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            <Form />
+            <div className="absolute top-4 right-4 cursor-pointer" onClick={() => setOpen(false)}>
+              <Image src="/close.png" alt="" height={14} width={14} />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
