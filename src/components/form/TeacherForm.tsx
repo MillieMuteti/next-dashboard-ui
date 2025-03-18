379703bd3febe00e6afbf32ab70e2c1b8541000f
Millie Muteti <milliemuteti@gmail.com>
2025-03-18 21:43:52 +0300

    Add forfm submit button

--- src/components/form/TeacherForm.tsx
+++ src/components/form/TeacherForm.tsx
@@ -7,7 +7,7 @@ import { z } from "zod";
 const schema = z.object({
   username: z.string()
   .min(3, { message: 'User name must be atleast 3  characters long!' })
-  .max(1, { message: 'User name must not exceed 20 characters' }),
+  .max(20, { message: 'User name must not exceed 20 characters' }),
   email: z.string().email({message:"Invalid Email Address"}),
   password: z.string()
   .min(8, { message: 'User name must be atleast 8 characters long!' }),
@@ -28,13 +28,18 @@ const TeacherForm = ({ type, data }: { type: "create" | "update"; data?:any } ) 
       } = useForm({
         resolver: zodResolver(schema),
       });
+
+      const onSubmit = handleSubmit(data=>{
+        console.log(data);
+      })
   return (
-    <form className="flex flex-col gap-8">
+    <form className="flex flex-col gap-8" onSubmit={onSubmit}>
         <h1 className="text-xs text-gray-400 font-medium">Create a new teacher</h1>
         <span className=" text-xs text-gray-400 font-medium">Authentication Informartion</span>
         <input type="text" {...register("username")} className="ring-[1.5px] text-gray-300 p-2 rounded-md text-sm"/>
-        {errors.username?.message && <p>{errors.username?.message.toString()}</p>}
+        {errors.username?.message && <p className="text-xs text-red-400">{errors.username?.message.toString()}</p>}
         <span className="text-xs text-gray-400 font-medium">Personal Information</span>
+        <button className="bg-blue-400 text-white p-2 rounded-md">{type === "create" ?"Create" : "Update"}</button>
       
     </form>
   );
